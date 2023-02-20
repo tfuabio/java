0a63ce24967053aa7ddbe97e61cf14916da1e25a
tfuabio <114894082+tfuabio@users.noreply.github.com>
2023-02-20 10:16:24 +0900

    finish chapter6

---
+++ wcp/JavaWcp/src/Chapter06_practice.java
@@ -0,0 +1,12 @@
+
+public class Chapter06_practice {
+	public static void main(String[] args) {
+		String a = "applePie";
+		String b = "applePie";
+		System.out.println(a == b);
+		
+		String d = "apple";
+		String e = "Pie";
+		System.out.println(a.equals(d + e));
+	}
+}
